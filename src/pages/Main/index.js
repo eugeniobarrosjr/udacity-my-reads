6fadf3a9e7e50ffbd84cec51c45706749a7c364e
Eugênio <eugenio.barros@fortbrasil.com.br>
2018-10-25 14:17:08 -0300

    Implements BookShelf into Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,31 @@
 import React, { Fragment } from 'react';
 import propTypes from 'prop-types';
 
+import BookShelf from '../../components/BookShelf';
 import Header from '../../components/Header';
 import FloatingButton from '../../components/FloatingButton';
 
 const Main = ({ books, updateBookDetails }) => (
   <Fragment>
     <Header />
+    <BookShelf
+      books={books}
+      title="Currently Reading"
+      typeOfShelf="currentlyReading"
+      updateBookDetails={updateBookDetails}
+    />
+    <BookShelf
+      books={books}
+      title="Want to Read"
+      typeOfShelf="wantToRead"
+      updateBookDetails={updateBookDetails}
+    />
+    <BookShelf
+      title="Read"
+      books={books}
+      typeOfShelf="read"
+      updateBookDetails={updateBookDetails}
+    />
     <FloatingButton />
   </Fragment>
 );
