0b6d00902bf838a55c1d774b228f525f89a84a64
Eugênio <eugenio.barros@fortbrasil.com.br>
2018-10-25 14:39:12 -0300

    Add dist/

---
+++ dist/precache-manifest.83ec5efab414a471a2c42ca4119a899a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a0ece9c8197244ad3953ac46cc34431",
+    "url": "/udacity-my-reads/static/media/defbookcover.9a0ece9c.jpg"
+  },
+  {
+    "revision": "19776f8b824114bf3d3a",
+    "url": "/udacity-my-reads/static/js/runtime~main.19776f8b.js"
+  },
+  {
+    "revision": "8c830764899b15a6913f",
+    "url": "/udacity-my-reads/static/js/main.8c830764.chunk.js"
+  },
+  {
+    "revision": "cdb61fef69cb345e1f1e",
+    "url": "/udacity-my-reads/static/js/1.cdb61fef.chunk.js"
+  },
+  {
+    "revision": "c2fc4aa5230a10d846c96e322183e2cf",
+    "url": "/udacity-my-reads/index.html"
+  }
+];
